8085668f569e97ad72240a4f2084f37a898989da
avivle <avivlevinson1992@gmail.com>
2021-03-17 10:46:02 +0200

    create img picker

--- src/components/TakePicture.js
+++ src/components/TakePicture.js
@@ -1,6 +1,8 @@
+/*
+
 import React, {useState} from 'react';
 
-import {View, Button, Text, StyleSheet, Image, Alert} from 'react-native';
+import {View,  Text, StyleSheet, Image, Alert,TouchableOpacity} from 'react-native';
 
 import * as ImagePicker from 'expo-image-picker'
 import * as Promissions from 'expo-permissions';
@@ -38,29 +40,154 @@ export default function TakePicture(props){
 
     return(
         <View style={styles.imagePicker}>
+
+
+
+        </View>
+    );
+}
+
+
+
+
+const styles = StyleSheet.create({
+    imagePicker:{
+        flex:1,
+    },
+    imagePreview:{
+        width:'100%',
+        flex:0.5,
+        borderWidth:10,
+        borderColor:'black',
+        alignItems:'center',
+        marginTop:10,
+
+    },
+    image:{
+        width:"30%",
+        height:30,
+        marginBottom:5,
+
+    },
+    imageContainer:{
+        borderColor:'black',
+        borderWidth:10,
+        justifyContent:'space-around',
+        flexWrap:'wrap',
+        flexDirection:'row',
+        flex:1,
+        width:"100%",
+
+
+    },
+
+  btn: {
+    alignItems: 'center',
+    backgroundColor: '#4756DF',
+    borderRadius: 50,
+    width: 150,
+    height: 50,
+    elevation: 6,
+  },
+
+  textInsideButton: {
+    color: 'white',
+    fontSize: 20,
+    paddingTop: 5,
+  },
+  bottomContainer: {
+    flex: 2,
+  },
+});
+*/
+
+/**
+ * return(
+        <View style={styles.imagePicker}>
+
+
             <View style={styles.imagePreview}>
                 <Text>NO image picked yet. </Text>
-                </View>
-            <Image style={styles.image} source={{uri: imagePicker }}/>
-            <Button title="Take image" onPress = {takeImagePicker } />
+            </View>
+
+
+            <View style={styles.imageContainer}>
+
+            <Image style={styles.image} source={{uri: imagePicker }} resizeMode='contain'/>
+            <Image style={styles.image} source={{uri: imagePicker }} resizeMode='contain'/>
+            <Image style={styles.image} source={{uri: imagePicker }} resizeMode='contain'/>
+            <Image style={styles.image} source={{uri: imagePicker }} resizeMode='contain'/>
+            <Image style={styles.image} source={{uri: imagePicker }} resizeMode='contain'/>
+            </View>
+ 
+
+
+        <View style={styles.bottomContainer}>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() => {
+              console.log('press ');
+              takeImagePicker();
+            }}
+            activeOpacity={0.5}>
+            <Text style={styles.textInsideButton}> Take image </Text>
+          </TouchableOpacity>
+        </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     imagePicker:{
-        alignItems:'center',
+        flex:1,
     },
-    imagePreview:{},
+    imagePreview:{
+        width:'100%',
+        flex:0.5,
+        borderWidth:10,
+        borderColor:'black',
+        alignItems:'center',
+        marginTop:10,
+
+    },
     image:{
+        width:"30%",
+        height:30,
+        marginBottom:5,
+
+    },
+    imageContainer:{
+        borderColor:'black',
+        borderWidth:10,
+        justifyContent:'space-around',
+        flexWrap:'wrap',
+        flexDirection:'row',
+        flex:1,
         width:"100%",
-        height:200,
-        marginBottom:10,
-        justifyContent:"center",
-        alignItems:"center",
-        borderColor:'#ccc',
-        borderWidth:1,
 
 
-    }
+    },
+
+  btn: {
+    alignItems: 'center',
+    backgroundColor: '#4756DF',
+    borderRadius: 50,
+    width: 150,
+    height: 50,
+    elevation: 6,
+  },
+
+  textInsideButton: {
+    color: 'white',
+    fontSize: 20,
+    paddingTop: 5,
+  },
+  bottomContainer: {
+    flex: 2,
+  },
 });
+ * 
+ * 
+ * 
+ * 
+ */
--- src/screen/auth/PictureScreen.js
+++ src/screen/auth/PictureScreen.js
@@ -1,34 +1,145 @@
-import React, { useState } from 'react';
-import { View, Button, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Button, StyleSheet, Text, Image, TouchableOpacity, Alert } from 'react-native';
 
-import TakePicture from '../../components/TakePicture.js';
+//import TakePicture from '../../components/TakePicture.js';
 
+import useImgPicker from '../../components/useImgPicker.js'
+
+import snapImage from '../../../assets/img/snapImage.png';
 
 
 export default function PictureScreen({ navigation, route }) {
+
+  const { imgPicker , takeImagePicker} = useImgPicker();
+
+  const [ imgTest, setImgTest] = useState('');
+
+  const [hasTakePicture, setHasTakePicture] = useState(false);
+
+  const [urlList, setUrlList] = useState(null);
+
+
+
+  useEffect(()=>{
+    setImgTest((prev)=>{
+      prev:imgPicker})
+      console.log("bla - ",imgPicker)
+    
+  },[]);
+
+
+  const displayImg = async ()=>{
+
+    console.log('display img on screen');
+    const success = await takeImagePicker();
+    console.log('after take img picker');
+    if(!success){
+      Alert.alert('Error',
+      'cant show picture',
+      [{text: 'Okay'}]
+     );
+      return;
+    }
+
+
+    setHasTakePicture(true);
+
+
+
+  }
+
   return (
-    <View> 
-        <TakePicture/>
+    <View style={styles.container}>
+      <View style={styles.title}>
+        <Text>take a picture of your face</Text>
+        <Text>totle picture: (1/3)</Text>
+      </View>
+
+      <View style={styles.bodyContainer}>
+        <Image source={snapImage} style={styles.img} resizeMode="contain" />
+      </View>
+      {hasTakePicture ? (
+
+
+      <View style={styles.imgContainer}>
+    
+        <View>
+        <Image style={styles.image} source={{uri: imgPicker }} resizeMode='contain'/>
+        <Text>{imgPicker}</Text>
+        
+        </View>
+        </View>
+
+        ) :
+        (
+          <View></View>
+        )
+        }
+      
+      
+        <View style={styles.btnContainer}>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={ displayImg }
+            activeOpacity={0.5}>
+            <Text style={styles.textInsideButton}> take a picture </Text>
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
 
-
-const style = StyleSheet.create({
-  container:{
-    flex:1,
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
   },
-  googleSearch:{
-    flex:1,
+  title: {
+    marginTop: 10,
+    alignItems: 'center',
+    borderWidth: 5,
+    borderColor: 'black',
+  },
+  bodyContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    borderWidth: 5,
+    borderColor: '#FFFFFF',
+    flex: 0.5,
+    borderWidth: 1,
+  },
 
+  imgContainer: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    borderColor: 'black',
+    flex: 0.3,
+    borderWidth: 1,
   },
-  mpaContainer:{
-    flex:1,
+
+  img: {
+    width: '100%',
+    height: '100%',
   },
-  btnContainer:{
-    flex:1,
-    
-  }
+
+  btnContainer: {
+    flex: 0.2,
+    borderWidth: 5,
+    borderColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    alignItems: 'center',
+    backgroundColor: '#4756DF',
+    borderRadius: 50,
+    width: 150,
+    height: 50,
+    elevation: 6,
+  },
+  textInsideButton: {
+    color: 'white',
+    fontSize: 20,
+    paddingTop: 5,
+  },
 });
